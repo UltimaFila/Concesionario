edd11059ae6b09e8846839db1a7dcf0d9f72668c
Clarandom <clarasubiron@gmail.com>
2015-04-09 21:12:02 +0200

    Random

--- Coche.java
+++ Coche.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Coche {
 
 	String Marca;
@@ -8,14 +10,14 @@ public class Coche {
 	public Coche() {
 	}
 
-	public Coche(String Marca, String Modelo, String Bastidor, int Kilometraje)
+	public Coche(String Marca, String Modelo, int Kilometraje)
 
 	{
 
 		this.Marca = Marca;
 		this.Modelo = Modelo;
-		this.Bastidor = Bastidor;
 		this.Kilometraje = Kilometraje;
+		randomito(Marca.charAt(0));
 
 	}
 
@@ -52,7 +54,41 @@ public class Coche {
 	}
 
 	public String toString() {
-		return (this.Marca + this.Modelo + this.Bastidor + this.Kilometraje);
+		return (this.Marca + " " + this.Modelo + " " + this.Bastidor + " " + this.Kilometraje);
 
 	}
-}
+	
+	
+
+		public void randomito(char letra) {
+
+			String cadena1 = CadenaAlfanum(1);
+			String cadena2 = CadenaAlfanum(11);
+			
+			int randomito = (int) (Math.random()*(1000000-100000)+100000);
+			
+			
+			setBastidor(cadena1+letra+cadena2+randomito);
+			
+		}
+
+		static String CadenaAlfanum(int longitud) {
+			
+			
+			String cadenaAleatoria = "";
+			Random rm=new Random();
+			long m = rm.nextInt(1000000000);
+			Random r = new Random(m);
+			int i = 0;
+			while (i < longitud) {
+				char c = (char) r.nextInt(255);
+				if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z')) {
+					cadenaAleatoria += c;
+					i++;
+				}
+			}
+			return cadenaAleatoria;
+		}
+	}
+
+
--- MiConcesionario.java
+++ MiConcesionario.java
@@ -3,7 +3,10 @@ public class MiConcesionario {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Coche Cochecito = new Coche("Mercedes", "A4", 34324342);
 
+		System.out.println(Cochecito);
 	}
 
 }
